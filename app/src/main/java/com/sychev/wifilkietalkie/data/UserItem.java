b1731bc06457e7527d5071b6f4fe15c18c5e4f61
Eugeny Sychev <eugeny.sychev@gmail.com>
2020-12-13 22:11:34 +0300

    add initial abonent class

--- app/src/main/java/com/sychev/wifilkietalkie/data/UserItem.java
+++ app/src/main/java/com/sychev/wifilkietalkie/data/UserItem.java
@@ -1,5 +1,33 @@
 package com.sychev.wifilkietalkie.data;
 
-public class UserItem {
+import java.net.InetAddress;
 
+public class UserItem {
+    private String mUserName;
+    private InetAddress mUserAddress;
+    private int mNotReceivedCount;
+
+    public String getUserName() {
+        return mUserName;
+    }
+
+    public void setUserName(String userName) {
+        this.mUserName = userName;
+    }
+
+    public InetAddress getUserAddress() {
+        return mUserAddress;
+    }
+
+    public void setUserAddress(InetAddress userAddress) {
+        this.mUserAddress = userAddress;
+    }
+
+    public int getmNotReceivedCount() {
+        return mNotReceivedCount;
+    }
+
+    public void setmNotReceivedCount(int mNotReceivedCount) {
+        this.mNotReceivedCount = mNotReceivedCount;
+    }
 }
